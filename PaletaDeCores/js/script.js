be2489f1e3052bf7623ebb6c066fb66e2744cdfc
PriscilaSilvaAlves <priscila.contato@live.com>
2023-04-18 17:34:34 -0300

    Add files via upload

--- PaletaDeCores/js/script.js
+++ PaletaDeCores/js/script.js
@@ -1,16 +1,21 @@
 function createDivs(){
-  let num = document.getElementById('matriz').value;
+  var num = document.getElementById('matriz').value;
   if (num>100){
     alert("This number can't be rendered. The maximum value is 100.");
     num=100;
   }
-  let matriz = num*num;
-  let h=(512/num)-2;
-  let w=(512/num)-2;
-  let altura=h+"px";
-  let largura=w+"px";
-  let escrever='';
-  let colunas='';
+  if(window.innerHeight > window.innerWidth){
+    var h=(832/num)-2;
+    var w=(832/num)-2;
+  }else{
+    var h=(512/num)-2;
+    var w=(512/num)-2;
+  }
+  var matriz = num*num;
+  var altura=h+"px";
+  var largura=w+"px";
+  var escrever='';
+  var colunas='';
   for (var i = 0; i<matriz; i++) {
     escrever += `<div id="${i}" class="gridClass" quantidade="1"></div>`
   }
@@ -27,4 +32,23 @@ function createDivs(){
 function cleanDivs(){
   document.getElementById(`gd`).innerHTML = '';
   document.getElementById(`gd`).style.gridTemplateColumns="auto";
+}
+function selectRGB(cor,taxa){
+  if(cor=="preto"){
+    return `rgb(0,0,0,${taxa})`;
+  }else if(cor=="cinza"){
+    return `rgb(190,190,190,${taxa})`;
+  }else if(cor=="azul"){
+    return `rgb(0,0,255,${taxa})`;
+  }else if(cor=="vermelho"){
+    return `rgb(255,0,0,${taxa})`;
+  }else if(cor=="amarelo"){
+    return `rgb(255,255,0,${taxa})`;
+  }else if(cor=="rosa"){
+    return `rgb(255,0,255,${taxa})`;
+  }else if(cor=="verde"){
+    return `rgb(0,255,0,${taxa})`;
+  }else if(cor=="marrom"){
+    return `rgb(184,115,51,${taxa})`;
+  }
 }
