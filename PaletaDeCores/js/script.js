8f61b7f704d15e7862c1c3c314c184c3765343d1
PriscilaSilvaAlves <priscila.contato@live.com>
2023-04-21 18:19:16 -0300

    Add files via upload

--- PaletaDeCores/js/script.js
+++ PaletaDeCores/js/script.js
@@ -34,33 +34,65 @@ function cleanDivs(){
   document.getElementById(`gd`).style.gridTemplateColumns="auto";
 }
 function selectRGB(cor,taxa){
-  if(cor=="preto"){
-    return `rgb(0,0,0,${taxa})`;
-  }else if(cor=="cinza"){
-    return `rgb(190,190,190,${taxa})`;
-  }else if(cor=="azul"){
-    return `rgb(0,0,255,${taxa})`;
-  }else if(cor=="ciano"){
-    return `rgb(0,255,255,${taxa})`
-  }else if(cor=="vermelho"){
-    return `rgb(255,0,0,${taxa})`;
-  }else if(cor=="amarelo"){
-    return `rgb(255,255,0,${taxa})`;
-  }else if(cor=="pink"){
-    return `rgb(255,28,174,${taxa})`;
-  }else if(cor=="rosa"){
-    return `rgb(255,0,255,${taxa})`;
-  }else if(cor=="verde"){
-    return `rgb(33,94,33,${taxa})`;
-  }else if(cor=="verdeLimao"){
-    return `rgb(0,255,0,${taxa})`;
-  }else if(cor=="marrom"){
-    return `rgb(184,115,51,${taxa})`;
-  }else if(cor=="laranja"){
-    return `rgb(255,165,0,${taxa})`;
-  }else if(cor=="rosaBebe"){
-    return `rgb(233,194,166,${taxa})`;
-  }else if(cor=="bege"){
-    return `rgb(234,234,174,${taxa})`;
+  if(taxa==="1"){
+    if(cor=="preto"){
+      return `rgb(0, 0, 0)`;
+    }else if(cor=="cinza"){
+      return `rgb(190, 190, 190)`;
+    }else if(cor=="azul"){
+      return `rgb(0, 0, 255)`;
+    }else if(cor=="ciano"){
+      return `rgb(0, 255, 255)`
+    }else if(cor=="vermelho"){
+      return `rgb(255, 0, 0)`;
+    }else if(cor=="amarelo"){
+      return `rgb(255, 255, 0)`;
+    }else if(cor=="pink"){
+      return `rgb(255, 28, 174)`;
+    }else if(cor=="rosa"){
+      return `rgb(255, 0, 255)`;
+    }else if(cor=="verde"){
+      return `rgb(33, 94, 33)`;
+    }else if(cor=="verdeLimao"){
+      return `rgb(0, 255, 0)`;
+    }else if(cor=="marrom"){
+      return `rgb(184, 115, 51)`;
+    }else if(cor=="laranja"){
+      return `rgb(255, 165, 0)`;
+    }else if(cor=="rosaBebe"){
+      return `rgb(233, 194, 166)`;
+    }else if(cor=="bege"){
+      return `rgb(234, 234, 174)`;
+    }
+  }else{
+    if(cor=="preto"){
+      return `rgba(0, 0, 0, ${taxa})`;
+    }else if(cor=="cinza"){
+      return `rgba(190, 190, 190, ${taxa})`;
+    }else if(cor=="azul"){
+      return `rgba(0, 0, 255, ${taxa})`;
+    }else if(cor=="ciano"){
+      return `rgba(0, 255, 255, ${taxa})`
+    }else if(cor=="vermelho"){
+      return `rgba(255, 0, 0, ${taxa})`;
+    }else if(cor=="amarelo"){
+      return `rgba(255, 255, 0, ${taxa})`;
+    }else if(cor=="pink"){
+      return `rgba(255, 28, 174, ${taxa})`;
+    }else if(cor=="rosa"){
+      return `rgba(255, 0, 255, ${taxa})`;
+    }else if(cor=="verde"){
+      return `rgba(33, 94, 33, ${taxa})`;
+    }else if(cor=="verdeLimao"){
+      return `rgba(0, 255, 0, ${taxa})`;
+    }else if(cor=="marrom"){
+      return `rgba(184, 115, 51, ${taxa})`;
+    }else if(cor=="laranja"){
+      return `rgba(255, 165, 0, ${taxa})`;
+    }else if(cor=="rosaBebe"){
+      return `rgba(233, 194, 166, ${taxa})`;
+    }else if(cor=="bege"){
+      return `rgba(234, 234, 174, ${taxa})`;
+    }
   }
 }
