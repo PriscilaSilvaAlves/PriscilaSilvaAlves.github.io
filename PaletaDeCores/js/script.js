2a24db62cfc2958bfca1ce72c893dd90b2173b00
PriscilaSilvaAlves <priscila.contato@live.com>
2023-04-21 17:39:03 -0300

    Add files via upload

--- PaletaDeCores/js/script.js
+++ PaletaDeCores/js/script.js
@@ -40,15 +40,27 @@ function selectRGB(cor,taxa){
     return `rgb(190,190,190,${taxa})`;
   }else if(cor=="azul"){
     return `rgb(0,0,255,${taxa})`;
+  }else if(cor=="ciano"){
+    return `rgb(0,255,255,${taxa})`
   }else if(cor=="vermelho"){
     return `rgb(255,0,0,${taxa})`;
   }else if(cor=="amarelo"){
     return `rgb(255,255,0,${taxa})`;
+  }else if(cor=="pink"){
+    return `rgb(255,28,174,${taxa})`;
   }else if(cor=="rosa"){
     return `rgb(255,0,255,${taxa})`;
   }else if(cor=="verde"){
+    return `rgb(33,94,33,${taxa})`;
+  }else if(cor=="verdeLimao"){
     return `rgb(0,255,0,${taxa})`;
   }else if(cor=="marrom"){
     return `rgb(184,115,51,${taxa})`;
+  }else if(cor=="laranja"){
+    return `rgb(255,165,0,${taxa})`;
+  }else if(cor=="rosaBebe"){
+    return `rgb(233,194,166,${taxa})`;
+  }else if(cor=="bege"){
+    return `rgb(234,234,174,${taxa})`;
   }
 }
