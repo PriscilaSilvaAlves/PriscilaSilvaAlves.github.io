3aeb08e0d7e849ed950a47d6731b3f7d596c026e
PriscilaSilvaAlves <priscila.contato@live.com>
2023-05-08 19:12:34 -0300

    Delete encryptDecrypt/javascript directory

--- encryptDecrypt/javascript/src/components/footer.js
+++
@@ -1,8 +0,0 @@
-function Footer(){
-    return (
-        <footer>
-            <p>Desenvolvido por Priscila Alves, e-mail: priscila.contato@live.com</p>
-        </footer>
-    );
-}
-export default Footer;
