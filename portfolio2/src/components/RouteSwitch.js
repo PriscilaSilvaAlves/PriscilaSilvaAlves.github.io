c1d5f6c92d50e3ac902650683e354e1f40be1fb6
PriscilaSilvaAlves <priscila.contato@live.com>
2023-06-24 17:51:25 -0300

    Add files via upload

--- portfolio2/src/components/RouteSwitch.js
+++ portfolio2/src/components/RouteSwitch.js
@@ -1,13 +1,15 @@
+import React from 'react';
 import { Routes, Route, HashRouter } from "react-router-dom";
 import App from "../App";
 import Admin from "./admin";
 
 function RouteSwitch(){
     return (
-        <HashRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
             <Routes>    
-                <Route path="/" element={<App />} />
-                <Route path="/admin" element={<Admin />} />
+                <Route exac path="/" element={<App />} />
+                <Route exact path="/#quem-e" element={<App />} />
+                <Route exact path="/admin" element={<Admin />} />
             </Routes>
         </HashRouter>
     );
