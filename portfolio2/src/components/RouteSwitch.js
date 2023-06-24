1f2cd49e72ccd30d51b86491543a48151af94948
PriscilaSilvaAlves <priscila.contato@live.com>
2023-06-24 19:22:22 -0300

    Add files via upload

--- portfolio2/src/components/RouteSwitch.js
+++ portfolio2/src/components/RouteSwitch.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
 import App from "../App";
 import Admin from "./admin";
 
